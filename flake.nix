4993330156840ac56aae136f0752f2c3f1b02224
miyaco <miyako.dev@gmail.com>
2026-01-16 14:22:57 +0900

    [rio] use latest flake

--- flake.nix
+++ flake.nix
@@ -9,11 +9,12 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    rio.url = "github:raphamorim/rio/main";
   };
 
 
   outputs =
-    { self, nixpkgs, nixgl, home-manager, ... }:
+    { self, nixpkgs, nixgl, home-manager, rio, ... }:
     let
       system = "x86_64-linux";
 
@@ -31,6 +32,12 @@
         # the path to your home.nix.
         modules = [
           ./home.nix
+          ({ pkgs, ... }: {
+            programs.rio = {
+              enable = true;
+              package = rio.packages.${pkgs.system}.rio;
+            };
+          })
         ];
       };
     };

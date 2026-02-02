261bd92f30c1dfd425f3ccbdfab2e2d20400329e
miyaco <miyako.dev@gmail.com>
2026-02-02 10:58:22 +0900

    [fish] use macchina on wezterm startup

--- fish/fish.nix
+++ fish/fish.nix
@@ -167,7 +167,7 @@
       set -x LS_COLORS 'di=01;34:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 
       # fetch
-      if test "$TERM" = xterm-ghostty; and type -q macchina
+      if string match --quiet --regex '.*(ghostty|wezterm).*' "$TERM"; and type -q macchina
         macchina
       else if type -q fastfetch
         fastfetch

5a4cd5ebbf94320d5886697a7dab08eeef47f92f
miyaco <miyako.dev@gmail.com>
2026-02-04 06:50:00 +0900

    [ghostty] configure in nix
    Nix enables systemd-service automatically

--- ghostty/ghostty.nix
+++ ghostty/ghostty.nix
@@ -1,10 +1,38 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [
-    ghostty
-  ];
+  programs.ghostty = {
+    enable = true;
+    enableFishIntegration = true;
+    systemd.enable = true;
+    settings = {
+      font-family = [ "Google Sans Code" "IBM Plex Sans JP" ];
+      font-size = 15.0;
 
-  home.file.".config/ghostty/config" = {
-    source = ./config;
+      font-variation = "wght=300";
+      font-variation-bold = "wght=600";
+      font-variation-italic = "wght=300";
+      font-variation-bold-italic = "wght=600";
+
+      theme = "kanagawa-dragon";
+      background-opacity = 0.92;
+
+      command = "fish";
+
+      shell-integration = "fish";
+      shell-integration-features = "cursor,sudo,no-title";
+
+      window-inherit-working-directory = false;
+      working-directory = "home";
+
+      cursor-style = "block";
+      cursor-style-blink = false;
+      cursor-opacity = 0.9;
+
+      window-decoration = false;
+      window-padding-x = 12;
+      window-padding-y = 12;
+
+      quit-after-last-window-closed = false;
+    };
   };
   home.file.".config/ghostty/themes" = {
     source = ./themes;

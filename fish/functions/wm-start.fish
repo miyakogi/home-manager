bfee81ad0d9f32f0d2c4401e2390d407f08cb9c1
miyaco <miyako.dev@gmail.com>
2026-02-04 17:23:05 +0900

    [fish] refine login function

--- fish/functions/wm-start.fish
+++ fish/functions/wm-start.fish
@@ -1,11 +1,17 @@
 #!/usr/bin/env fish
 
 function wm-start
-  if command -q uwsm
-    exec uwsm start (string lower $argv[1]).desktop
+  if type -q uwsm
+    if test "$argv[1]" = "Hyprland"
+      exec uwsm start hyprland.desktop
+    else if "$argv[1]" = "niri"
+      exec uwsm start niri
+    else
+      exec uwsm start (string lower $argv[1]).desktop
+    end
   else if test "$argv[1]" = "Hyprland"
     exec start-hyprland
-  else if test $argv[1] = niri
+  else if test "$argv[1]" = "niri"
     exec niri-session
   else
     exec $argv[1]

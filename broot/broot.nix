9129aebb81a3cd9b14327f1b3c94836d219d5450
miyaco <miyako.dev@gmail.com>
2026-03-31 09:56:51 +0900

    [broot] configure kitty graphics protocol

--- broot/broot.nix
+++ broot/broot.nix
@@ -4,6 +4,8 @@
     enableFishIntegration = true;
     settings = {
       default_flags = "-c :open_preview";
+      kitty_graphics_transmission = "chunks";
+      kitty_graphics_display = "direct";
     };
   };
 }

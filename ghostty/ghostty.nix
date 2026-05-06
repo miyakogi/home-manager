6a22eb29d9ad4e7bce5ab661bb35db7e32a1b457
miyaco <miyako.dev@gmail.com>
2026-05-06 14:20:54 +0900

    [ghostty] change font to Lilex

--- ghostty/ghostty.nix
+++ ghostty/ghostty.nix
@@ -5,13 +5,18 @@
     enableFishIntegration = true;
     systemd.enable = true;
     settings = {
-      font-family = [ "Google Sans Code" "IBM Plex Sans JP" ];
+      font-family = [ "Lilex" "IBM Plex Sans JP" ];
       font-size = 15.0;
+      font-feature = "+cv08 +cv11 +cv15";
 
       font-variation = "wght=300";
       font-variation-bold = "wght=600";
       font-variation-italic = "wght=300";
       font-variation-bold-italic = "wght=600";
+      # font-style = "Light";
+      font-style-bold = "Medium";
+      # font-style-italic = "Light";
+      font-style-bold-italic = "Medium";
 
       theme = "kanagawa-dragon";
       background-opacity = 0.92;

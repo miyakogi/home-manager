0d480b1bc3271a5c91351a6028b8b938f50f31eb
miyaco <miyako.dev@gmail.com>
2026-05-07 15:06:44 +0900

    [fish] add `img` alias

--- fish/fish.nix
+++ fish/fish.nix
@@ -173,6 +173,14 @@
       set -x LSCOLORS Exfxcxdxbxegedabagacad
       set -x LS_COLORS 'di=01;34:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 
+      # image viewer
+      switch $TERM
+          case kitty xterm-ghostty xterm-rio
+              alias img="kitten icat"
+          case wezterm
+              alias img="wezterm imgcat"
+      end
+
       # load machine local setting
       if test -f ~/.config/fish/local.fish
         source ~/.config/fish/local.fish

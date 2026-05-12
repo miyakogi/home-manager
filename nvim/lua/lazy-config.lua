cea5a7d5a5e63fe7a76e7e5a4957d72f6ffb179b
miyaco <miyako.dev@gmail.com>
2026-05-12 09:51:15 +0900

    [nvim] update treesitter to latest

--- nvim/lua/lazy-config.lua
+++ nvim/lua/lazy-config.lua
@@ -9,61 +9,7 @@ local plugins = {
 
   {
     'nvim-treesitter/nvim-treesitter',
-    branch = 'master',
     build = ':TSUpdate',
-    config = function ()
-      require('nvim-treesitter.configs').setup({
-        -- A list of parser names, or 'all'
-        ensure_installed = {
-          'bash',
-          'c',
-          'cpp',
-          'dart',
-          'dockerfile',
-          'gitignore',
-          'html',
-          -- 'javascript',
-          'json',
-          'jsonc',
-          'lua',
-          'make',
-          'markdown',
-          'markdown_inline',
-          'python',
-          'regex',
-          'rst',
-          'rust',
-          'toml',
-          'typescript',
-          'vim',
-          'vimdoc',
-          'vue',
-          'yaml',
-        },
-
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-
-        -- Automatically install missing parsers when entering buffer
-        auto_install = true,
-
-        -- List of parsers to ignore installing (for "all")
-        ignore_install = {
-          'javascript',
-        },
-
-        highlight = {
-          -- `false` will disable the whole extension
-          enable = true,
-
-          disable = {},
-
-          additional_vim_regex_highlighting = {
-            'markdown',  -- required for zk-nvim
-          },
-        },
-      })
-    end,
   },
 
   {

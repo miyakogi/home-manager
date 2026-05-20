2f5973864844c325d3f1ef5a699abec1cb13590a
miyaco <miyako.dev@gmail.com>
2026-05-21 03:23:35 +0900

    [ghostty] change theme

--- ghostty/ghostty.nix
+++ ghostty/ghostty.nix
@@ -21,8 +21,8 @@
       # font-style-italic = "Light";
       font-style-bold-italic = "Medium";
 
-      theme = "kanagawa-dragon";
-      background-opacity = 0.92;
+      theme = "mikado";
+      background-opacity = 0.87;
 
       command = "fish";
 

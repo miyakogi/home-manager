667183be5c9c581a42ff5aa8007c1ed06c002eed
miyaco <miyako.dev@gmail.com>
2026-08-14 10:49:24 +0900

    [fish] split init scripts into separate files

---
+++ fish/interactive-shell.fish
@@ -0,0 +1,21 @@
+set -x GPG_TTY (tty)
+
+# need to activate auto ls function
+type -q __auto_ls
+
+# ls color setting
+set -x LSCOLORS Exfxcxdxbxegedabagacad
+set -x LS_COLORS 'di=01;34:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=46;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+
+# image viewer
+switch $TERM
+  case kitty xterm-ghostty xterm-rio
+    alias img="kitten icat"
+  case wezterm
+    alias img="wezterm imgcat"
+end
+
+# load machine local setting
+if test -f ~/.config/fish/local.fish
+  source ~/.config/fish/local.fish
+end

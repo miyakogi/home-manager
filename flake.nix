c9429f3f48fa4eea168e466fe0bb462feb2985b1
miyaco <miyako.dev@gmail.com>
2026-08-21 10:11:36 +0900

    Update seasalt

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
       url = "github:miyakogi/Anime4K-mpv-glsl";
       flake = false;
     };
-    seasalt.url = "github:miyakogi/seasalt/v0.2.1";
+    seasalt.url = "github:miyakogi/seasalt/v0.2.2";
   };
 
 

828906677fd09eef739ed9a8a801553bb9a306a8
miyaco <miyako.dev@gmail.com>
2026-08-28 03:11:38 +0900

    [kitty] change theme to blackmetal-ash

--- kitty/kitty.nix
+++ kitty/kitty.nix
@@ -6,14 +6,17 @@
   home.file.".config/kitty/kitty.conf" = {
     source = ./kitty.conf;
   };
+  home.file.".config/kitty/blackmetal-ash.conf" = {
+    source = ./blackmetal-ash.conf;
+  };
+  home.file.".config/kitty/hybrid.conf" = {
+    source = ./hybrid.conf;
+  };
   home.file.".config/kitty/kanagawa_dragon.conf" = {
     source = ./kanagawa_dragon.conf;
   };
   home.file.".config/kitty/mikado.conf" = {
     source = ./mikado.conf;
-  };
-  home.file.".config/kitty/hybrid.conf" = {
-    source = ./hybrid.conf;
   };
   home.file.".config/kitty/opencode.conf" = {
     source = ./opencode.conf;

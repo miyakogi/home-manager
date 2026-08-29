3ca871fc25e97a520e900bca5fd09379a29bdc8b
miyaco <miyako.dev@gmail.com>
2026-08-29 13:16:30 +0900

    [flatpak] comment

--- flatpak/flatpak.nix
+++ flatpak/flatpak.nix
@@ -2,47 +2,44 @@
   services.flatpak = {
     enable = true;
     packages = [
-      ### Browser
       # Firefox
-      "org.mozilla.firefox"           # Firefox
-      "app.zen_browser.zen"           # Zen
+      "org.mozilla.firefox"            # Firefox
+      "app.zen_browser.zen"            # Zen
 
       # Chromium
-      "org.chromium.Chromium"         # Chromium
-      "com.google.Chrome"             # Google Chrome
-      "com.brave.Browser"             # Brave
-      "com.vivaldi.Vivaldi"           # Vivaldi
+      "org.chromium.Chromium"          # Chromium
+      "com.google.Chrome"              # Google Chrome
+      "com.brave.Browser"              # Brave
+      "com.vivaldi.Vivaldi"            # Vivaldi
 
       # AI
-      # "ai.lmstudio.lm-studio"         # LM-Studio
-      # "ai.opencode.opencode"          # OpenCode Desktop
+      #"ai.lmstudio.lm-studio"         # LM-Studio
+      #"ai.opencode.opencode"          # OpenCode Desktop
 
-      ### Tools
       # Flatpak Management
-      "com.github.tchx84.Flatseal"    # Flatseal
+      "com.github.tchx84.Flatseal"     # Flatseal
 
       # E-mail
-      # "com.tutanota.Tutanota"         # Tuta Mail
+      #"com.tutanota.Tutanota"         # Tuta Mail
 
       # Note Taking
-      # "com.logseq.Logseq"             # Logseq
-      # "md.obsidian.Obsidian"          # Obsidian
+      #"com.logseq.Logseq"             # Logseq
+      #"md.obsidian.Obsidian"          # Obsidian
 
       # Task Management
-      # "com.ticktick.TickTick"         # TickTick
+      #"com.ticktick.TickTick"         # TickTick
 
-      ### Multimedia
       # Music Player
-      "com.spotify.Client"            # Spotify
+      "com.spotify.Client"             # Spotify
 
       # Game
-      "com.valvesoftware.Steam"       # Steam
+      "com.valvesoftware.Steam"        # Steam
       "com.valvesoftware.Steam.CompatibilityTool.Proton-GE"  # Proton-GE
 
       # Others
-      "com.obsproject.Studio"           # OBS
+      "com.obsproject.Studio"          # OBS
       "org.gnome.gitlab.dqpb.GMetronome"  # GMetronome
-      "org.kde.krita"                 # Krita
+      "org.kde.krita"                  # Krita
     ];
   };
 }

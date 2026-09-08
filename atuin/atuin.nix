feeec9b16ac6df18c5df6bf3f2c43e9798ecf618
miyaco <miyako.dev@gmail.com>
2026-09-08 09:44:06 +0900

    [atuin] include config

--- atuin/atuin.nix
+++ atuin/atuin.nix
@@ -4,6 +4,28 @@
     enableBashIntegration = true;
     enableFishIntegration = true;
     enableZshIntegration = true;
-    flags = [ "--disable-up-arrow" ];
+    flags = [
+      "--disable-up-arrow"
+    ];
+    settings = {
+      enter_accept = false;
+      search_mode = "daemon-fuzzy";
+      search = {
+        shells = "all";
+        filters = [
+          "directory"
+          "session"
+          "global"
+          "host"
+        ];
+      };
+      ai = {
+        enabled = true;
+      };
+      daemon = {
+        enabled = true;
+        autostart = true;
+      };
+    };
   };
 }

bdc20b4d11de14522db8b07b5e71e45c89579c06
miyaco <miyako.dev@gmail.com>
2026-09-09 12:14:43 +0900

    [rio] use hybrid theme on niri

--- rio/rio.nix
+++ rio/rio.nix
@@ -35,6 +35,6 @@ in {
     source = ./config.toml;
   };
   home.file.".config/rio-niri/themes/wm-theme.toml" = {
-    source = ./themes/blackmetal-ash.toml;
+    source = ./themes/hybrid.toml;
   };
 }

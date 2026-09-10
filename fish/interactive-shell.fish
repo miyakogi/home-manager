b208c81e6b350b1fa3a9ffac256c17155df273f5
miyaco <miyako.dev@gmail.com>
2026-09-11 06:00:11 +0900

    [fish] use zsh/bash on herdr env

--- fish/interactive-shell.fish
+++ fish/interactive-shell.fish
@@ -1,4 +1,4 @@
-if [ -z "$__fish_execute_once" ]
+if [ -z "$__fish_execute_once" ] || [ -n "$HERDR_ENV" ]
   export __fish_execute_once=1
   if [ "$XDG_CURRENT_DESKTOP" = Hyprland ] && [ "$TERM" != xterm-ghostty ]
     exec zsh

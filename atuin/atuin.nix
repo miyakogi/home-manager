d240493bdc4812e9ce526da111cf1db2327fb22c
miyaco <miyako.dev@gmail.com>
2026-09-23 18:47:13 +0900

    [atuin] enable up-arrow binding

--- atuin/atuin.nix
+++ atuin/atuin.nix
@@ -5,11 +5,16 @@
     enableFishIntegration = true;
     enableZshIntegration = true;
     flags = [
-      "--disable-up-arrow"
+      # "--disable-up-arrow"
     ];
     settings = {
       enter_accept = false;
       search_mode = "daemon-fuzzy";
+      search_mode_shell_up_key_binding = "prefix";
+      filter_mode_shell_up_key_binding = "directory";
+      inline_height_shell_up_key_binding = 16;
+      show_preview = true;
+      style = "full";
       search = {
         shells = "all";
         filters = [
@@ -18,9 +23,6 @@
           "session"
           "host"
         ];
-      };
-      ai = {
-        enabled = true;
       };
       daemon = {
         enabled = true;
